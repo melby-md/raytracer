2b50108125e56d0f706aa7631154500bd53bd4c3
Pedro Debs <pedrodebs1@gmail.com>
2025-09-02 09:35:00 -0300

    Changed optimization level

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 : "${CXX:=c++}"
 set -x
-${CXX} -o raytracer main.cpp -pipe -std=c++11 -fno-exceptions -fno-rtti -fopenmp -g3 -Wpedantic -Wall -Wextra -Wshadow -Wconversion -Wdeprecated -Wdouble-promotion -Wno-unused-parameter -Wno-unused-function -Wno-sign-conversion -lm
+${CXX} -o raytracer main.cpp -Og -pipe -std=c++11 -fno-exceptions -fno-rtti -fopenmp -g3 -Wpedantic -Wall -Wextra -Wshadow -Wconversion -Wdeprecated -Wdouble-promotion -Wno-unused-parameter -Wno-unused-function -Wno-sign-conversion -lm
