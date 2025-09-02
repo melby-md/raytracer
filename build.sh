d0b2b18eba82567ea9ae63169ad05fd75ed7e9d2
Pedro Debs <pedrodebs1@gmail.com>
2025-09-02 09:40:15 -0300

    Changed build script

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh -e
 : "${CXX:=c++}"
+FLAGS='-Og'
+if [ "$1" = 'release' ]; then
+	FLAGS='-O3 -DRELEASE'
+fi
 set -x
-${CXX} -o raytracer main.cpp -Og -pipe -std=c++11 -fno-exceptions -fno-rtti -fopenmp -g3 -Wpedantic -Wall -Wextra -Wshadow -Wconversion -Wdeprecated -Wdouble-promotion -Wno-unused-parameter -Wno-unused-function -Wno-sign-conversion -lm
+${CXX} -o raytracer main.cpp $FLAGS -pipe -std=c++11 -fno-exceptions -fno-rtti -fopenmp -g3 -Wpedantic -Wall -Wextra -Wshadow -Wconversion -Wdeprecated -Wdouble-promotion -Wno-unused-parameter -Wno-unused-function -Wno-sign-conversion -lm
